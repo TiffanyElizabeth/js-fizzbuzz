2ae7e30fe5a1988a49ea42bb1ea2b8408f321aa9
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-04 18:26:13 +0100

    feat: add fizzbuzz

--- script.js
+++ script.js
@@ -6,3 +6,16 @@
 // 2. se e divisibile da 3, stampa "Fizz" al posto del numero
 // 3. se e divisibile da 5, stampa "Buzz" al posto del numbero
 // 4. se e divisibile da 3 e da 5, deve essere divisibile da 15 - se il numere e divisibile da 15, stampa FizzBuzz
+// 5. altrimenti stampa il numero 
+
+for(let i=1; i<101; i++) {
+    if(i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    }
+}

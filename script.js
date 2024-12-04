e9fd1d16dcedf8134509306874d9b375cb3cf8b8
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-04 18:14:57 +0100

    feat: breakdown of steps

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// 1. loop min 1 max 100 (for)
+// 2. se e divisibile da 3, stampa "Fizz" al posto del numero
+// 3. se e divisibile da 5, stampa "Buzz" al posto del numbero
+// 4. se e divisibile da 3 e da 5, deve essere divisibile da 15 - se il numere e divisibile da 15, stampa FizzBuzz
